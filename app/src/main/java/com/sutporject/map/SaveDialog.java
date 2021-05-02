9e13829f93bceaffc627d9c3602c1bf5a5161e5c
Mohammad Abolnejadian <mohammad1379_a@yahoo.com>
2021-05-02 20:58:53 +0430

    Save dialog design improvement + no bookmark label shown if there is no bookmark available

--- app/src/main/java/com/sutporject/map/SaveDialog.java
+++ app/src/main/java/com/sutporject/map/SaveDialog.java
@@ -30,7 +30,8 @@ public class SaveDialog  extends AppCompatDialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater=getActivity().getLayoutInflater();
         View view=inflater.inflate(R.layout.save_dialog,null);
-        ((TextView)view.findViewById(R.id.location_textView)).setText(latLng.toString());
+        ((TextView)view.findViewById(R.id.location_lat)).setText(String.format("%.3f",latLng.getLatitude()));
+        ((TextView)view.findViewById(R.id.location_long)).setText(String.format("%.3f",latLng.getLongitude()));
         builder.setView(view).setTitle(R.string.save_dialog).setNegativeButton("cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
@@ -41,6 +42,7 @@ public class SaveDialog  extends AppCompatDialogFragment {
             public void onClick(DialogInterface dialogInterface, int i) {
                 String locationName=((EditText)view.findViewById(R.id.location_name)).getText().toString();
                 Bookmark.addBookmark(new Bookmark(locationName,latLng));
+                Toast.makeText(getContext(),"Location " + locationName + " has been added successfully.",Toast.LENGTH_SHORT).show();
             }
         });
         return builder.create();
