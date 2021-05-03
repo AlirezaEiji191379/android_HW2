e0c04585dc2e147df1d5297332bdd33e1227beea
Mohammad Abolnejadian <mohammad1379_a@yahoo.com>
2021-05-03 13:11:44 +0430

    Bookmarks search done

--- app/src/main/java/com/sutporject/map/BookmarkCardsAdapter.java
+++ app/src/main/java/com/sutporject/map/BookmarkCardsAdapter.java
@@ -1,9 +1,12 @@
 package com.sutporject.map;
 
 import android.annotation.SuppressLint;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,13 +14,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.sutporject.map.Model.Bookmark;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class BookmarkCardsAdapter extends RecyclerView.Adapter<BookmarkCardsAdapter.BookmarkViewHolder> {
+public class BookmarkCardsAdapter extends RecyclerView.Adapter<BookmarkCardsAdapter.BookmarkViewHolder> implements Filterable{
+    private static final String TAG = "Filter";
     private List<Bookmark> bookmarks;
+    private List<Bookmark> bookmarksFull;
 
     public BookmarkCardsAdapter(List<Bookmark> bookmarks) {
         this.bookmarks = bookmarks;
+        bookmarksFull = new ArrayList<>(bookmarks);
     }
 
     @NonNull
@@ -40,6 +47,43 @@ public class BookmarkCardsAdapter extends RecyclerView.Adapter<BookmarkCardsAdap
     public int getItemCount() {
         return bookmarks.size();
     }
+
+    @Override
+    public Filter getFilter() {
+        return bookmarkFilter;
+    }
+
+    private Filter bookmarkFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            List<Bookmark> filteredList = new ArrayList<>();
+            if(charSequence == null || charSequence.length() == 0)
+                filteredList.addAll(bookmarksFull);
+            else{
+                String pattern = charSequence.toString().trim().toLowerCase();
+                for (Bookmark bookmark : bookmarksFull) {
+                    if(bookmark.getName().trim().toLowerCase().contains(pattern))
+                        filteredList.add(bookmark);
+                }
+            }
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = filteredList;
+
+            Log.i(TAG, "perform: \n" + filteredList.toString());
+
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            bookmarks.clear();
+            bookmarks.addAll((List) filterResults.values);
+
+            Log.i(TAG, "publis: \n" + bookmarks.toString());
+
+            notifyDataSetChanged();
+        }
+    };
 
     class BookmarkViewHolder extends RecyclerView.ViewHolder{
 
--- app/src/main/java/com/sutporject/map/BookmarkFragment.java
+++ app/src/main/java/com/sutporject/map/BookmarkFragment.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.sutporject.map.Controller.SwipeController;
 import com.sutporject.map.Controller.SwipeControllerActions;
@@ -23,7 +24,9 @@ import java.util.ArrayList;
 public class BookmarkFragment extends Fragment {
     private boolean firstFlag = true;
     private RecyclerView bookmarksRecyclerView;
-    ItemTouchHelper itemTouchHelper;
+    private ItemTouchHelper itemTouchHelper;
+    private FloatingSearchView floatingSearchView;
+    private BookmarkCardsAdapter bookmarkCardsAdapter;
 
     public BookmarkFragment() {
         // Required empty public constructor
@@ -48,14 +51,17 @@ public class BookmarkFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_bookmark, container, false);
+
+        floatingSearchView = view.findViewById(R.id.search_bar_bookmarks);
         if(Bookmark.getBookmarks().size() != 0){
             TextView label = view.findViewById(R.id.no_bookmark_label);
             label.setVisibility(View.INVISIBLE);
+            label.setEnabled(false);
 
             bookmarksRecyclerView = view.findViewById(R.id.bookmarks_recyclerview);
             itemTouchHelper.attachToRecyclerView(bookmarksRecyclerView);
 
-            BookmarkCardsAdapter bookmarkCardsAdapter = new BookmarkCardsAdapter(Bookmark.getBookmarks());
+            bookmarkCardsAdapter = new BookmarkCardsAdapter(Bookmark.getBookmarks());
             bookmarksRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext(),
                     LinearLayoutManager.VERTICAL, false));
             bookmarksRecyclerView.setAdapter(bookmarkCardsAdapter);
@@ -63,7 +69,23 @@ public class BookmarkFragment extends Fragment {
                 Toast.makeText(view.getContext(),"Swipe left to delete",Toast.LENGTH_SHORT).show();
 
             firstFlag = true;
+
+            floatingSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
+                @Override
+                public void onSearchTextChanged(String oldQuery, String newQuery) {
+                    bookmarkCardsAdapter.getFilter().filter(newQuery);
+                }
+            });
+
+        }else{
+            floatingSearchView.setVisibility(View.INVISIBLE);
+            floatingSearchView.setEnabled(false);
         }
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
     }
 }
